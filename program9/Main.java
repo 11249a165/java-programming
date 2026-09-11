67a71bf57d3771cd7628447e21932c718080afd2
SAI ANIRUDDH <11249a165@kanchiuniv.ac.in>
2026-09-11 05:46:58 +0000

    program 9 commited

---
+++ program9/Main.java
@@ -0,0 +1,11 @@
+import studentInfo.Student;
+
+
+public class Main {
+     public static void main(String[] args) {
+
+        Student s1 = new Student("Rahul", 101, "BCA");
+
+        s1.displayInfo();
+    }
+}
