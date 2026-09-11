2d77c7e7b1d507d69266a8832f27eccf81b5123f
SAI ANIRUDDH <11249a165@kanchiuniv.ac.in>
2026-09-11 07:02:02 +0000

    counter thread with thread extending

---
+++ TaskManager.java
@@ -0,0 +1,24 @@
+class CounterThread extends Thread {
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+
+            try {
+                Thread.sleep(1000); // 1-second delay
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted");
+            }
+        }
+    }
+}
+
+public class TaskManager {
+
+    public static void main(String[] args) {
+
+        CounterThread thread = new CounterThread();
+
+        thread.start();
+    }
+}
